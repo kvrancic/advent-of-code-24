f01aff6ac0edc0708c0d2b4ef01694724b9fcc53
kvrancic <kvrancic11@gmail.com>
2024-12-01 21:21:29 +0100

    solved day 1 part 1

---
+++ day1/task1.py
@@ -0,0 +1,23 @@
+'''
+task: sum of element-wise difference between two sorted arrays
+'''
+
+with open('input.txt', 'r') as file:
+    rows = file.readlines()
+
+list1 = []
+list2 = []
+
+for r in rows: 
+    nums = r.split()
+    list1.append(int(nums[0]))
+    list2.append(int(nums[1]))
+
+list1.sort()
+list2.sort()
+
+sum = 0
+for i in range(len(list1)):
+    sum += abs(list1[i] - list2[i])
+
+print(sum)
